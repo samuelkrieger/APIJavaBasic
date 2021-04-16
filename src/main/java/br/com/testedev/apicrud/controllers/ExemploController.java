db46140243e68ed5e5b2b298e7d9124d0766ce8f
Jeferson Menchik <juniomenchik@live.com>
2021-04-16 19:39:56 -0300

    Remoção
    de duas paginas html sem uso

--- src/main/java/br/com/testedev/apicrud/controllers/ExemploController.java
+++ src/main/java/br/com/testedev/apicrud/controllers/ExemploController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ExemploController{
 
-    @GetMapping("/nohere")
+    @GetMapping("/")
     public String index(){
         return "index";
     }
