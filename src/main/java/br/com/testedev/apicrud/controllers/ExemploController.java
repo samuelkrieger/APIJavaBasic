e627155e6380365fbc683d44698a0b1590105de2
Jeferson Menchik <juniomenchik@live.com>
2021-04-19 14:37:38 -0300

    Atualizacao
    Atualizei

--- src/main/java/br/com/testedev/apicrud/controllers/ExemploController.java
+++ src/main/java/br/com/testedev/apicrud/controllers/ExemploController.java
@@ -1,14 +1,31 @@
 package br.com.testedev.apicrud.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ExemploController{
 
     @GetMapping("/")
-    public String index(){
-        return "index";
+    public String find(){
+        return "Voce acessou o metodo GET";
     }
+
+//    @GetMapping("/")
+//    public String findby(){
+//        return "Voce acessou o metodo GET BY =";
+//    }
+    @PostMapping("/")
+    public String post(){
+        return "Voce acessou o metodo POST";
+    }
+    @PutMapping("/")
+    public String put(){
+        return "Voce acessou o metodo PUT";
+    }
+
+    @DeleteMapping("/")
+    public String delete(){
+        return "Voce acessou o metodo DELETE";
+    }
+
 }
