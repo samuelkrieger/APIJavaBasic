ababf2513cc4cad9dba477f8dc20b5c1f2f5872d
Jeferson Menchik <juniomenchik@live.com>
2021-05-18 20:37:51 -0300

    ta tope ta tope os Cards

--- front/fazer/src/app/app.module.ts
+++ front/fazer/src/app/app.module.ts
@@ -7,19 +7,26 @@ import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { FooterComponent } from './components/footer/footer.component';
-
+import { ReadAllComponent } from './components/read-all/read-all.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ReadAllComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
